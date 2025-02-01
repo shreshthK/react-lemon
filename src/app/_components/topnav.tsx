a7346968906c5f0a6b6f54aa4112dc20f475c654
212601662 <shreshth.kandari@wabtec.com>
2025-02-01 01:26:51 -0600

    feat: add Clerk authentication with protected routes

---
+++ src/app/_components/topnav.tsx
@@ -0,0 +1,18 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
+
+export function TopNav() {
+
+    return (
+        <nav className="flex justify-between items-center w-full p-4 text-xl border-b">
+            <div>Gallery</div>
+            <div>
+                <SignedOut>
+                    <SignInButton />
+                </SignedOut>
+                <SignedIn>
+                    <UserButton />
+                </SignedIn>
+            </div>
+        </nav>
+    )
+}
