9e8a95ae099c55627f04c47af6f45af75ef4b018
212601662 <shreshth.kandari@wabtec.com>
2025-02-01 04:12:55 -0600

    chore: Configure ESLint with Next.js flat config and compatibility

---
+++ eslint.config.mjs
@@ -0,0 +1,17 @@
+import { FlatCompat } from '@eslint/eslintrc'
+
+const compat = new FlatCompat({
+  // import.meta.dirname is available after Node.js v20.11.0
+  baseDirectory: import.meta.dirname,
+})
+
+const eslintConfig = [
+  ...compat.config({
+    extends: ['next'],
+    rules: {
+      '@typescript-eslint/only-throw-error': 'off'
+    },
+  }),
+]
+
+export default eslintConfig
