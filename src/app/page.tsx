ce8cacb3cb8893d99d882cd09516e2f2bf62456d
212601662 <shreshth.kandari@wabtec.com>
2025-02-01 01:49:53 -0600

    feat: add authentication-based image gallery display

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,40 @@
 import { db } from "~/server/db";
-
+import { SignedOut, SignedIn } from "@clerk/nextjs";
 export const dynamic = 'force-dynamic';
 
-export default async function HomePage() {
+
+
+async function Images() {
   const images = await db.query.image.findMany({
     orderBy: (image, { desc }) => [desc(image.createdAt)]
   });
+
+  return (
+    <div className="flex flex-wrap gap-4">
+      {
+        images.map((image, index) => (
+          <div key={index} className="flex flex-col gap-2 w-48">
+            <img src={image.url} alt={image.name} />
+            <div className="text-sm text-gray-500">{image.name}</div>
+          </div>
+        ))
+      }
+    </div>
+  )
+}
+
+export default async function HomePage() {
   return (
     <main className="">
-      <div className="flex flex-wrap gap-4">
-        {
-          images.map((image,index) => ( 
-            <div key={index} className="flex flex-col gap-2 w-48">
-              <img src={image.url} alt={image.name} />
-              <div className="text-sm text-gray-500">{image.name}</div>
-            </div>
-          ))
-        }
-      </div>
+      <SignedOut>
+        <div className="flex flex-col items-center justify-center h-screen">
+          <h1 className="text-4xl font-bold">Welcome to React Lemon Gallery</h1>
+          <p className="text-lg">Please sign in to continue</p>
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
