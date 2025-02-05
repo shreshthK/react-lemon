7192c6d8720919055282e91dfa5995d0d1043220
212601662 <shreshth.kandari@wabtec.com>
2025-02-05 11:51:27 -0600

    fix: fix warning for "`params` should be awaited"

--- src/app/@modal/(.)img/[id]/page.tsx
+++ src/app/@modal/(.)img/[id]/page.tsx
@@ -1,11 +1,10 @@
 import { getImageById } from "~/server/queries";
 import { Modal } from "./modal";
-export default async function PhotoModal({
-  params: { id: photoId },
-}: {
+export default async function PhotoModal(props: {
   params: { id: string };
 }) {
-  const image = await getImageById(parseInt(photoId));
+  const { id } = await props.params;
+  const image = await getImageById(parseInt(id));
   return (
     <Modal>
       <img src={image.url} alt={image.name} width={380} height={380} />
--- src/app/img/[id]/page.tsx
+++ src/app/img/[id]/page.tsx
@@ -1,11 +1,10 @@
-export default async function PhotoModal({
-    params: { id: photoId },
-  }: {
-    params: { id: string };
-  }) {
-    return (
-      <div>
-        <div>{photoId}</div>
-      </div>
-    );
-  }
+import { getImageById } from "~/server/queries";
+export default async function PhotoModal(props: {
+  params: { id: string };
+}) {
+  const { id } = await props.params;
+  const image = await getImageById(parseInt(id));
+  return (
+      <img src={image.url} alt={image.name} width={380} height={380} />
+  );
+}
